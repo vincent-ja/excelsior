adc0baffe70eb4d92525a101a71e4eb7a57cd285
Vincent J. Accardo <vincent.j.accardo.0@gmail.com>
2021-10-31 01:40:11 -0400

    Implement inventory component
    * Add boilerplate game data

--- src/js/components.js
+++ src/js/components.js
@@ -1,22 +1,109 @@
 import React from "react";
+import * as GameData from "./gamedata";
 
 export class GameContainer extends React.Component{
+    state = {
+        activeOptions: [
+            "Continue walking",
+            "Inspect the object"
+        ],
+        inventory: [
+            {
+                id: 0
+            },
+            {
+                id: 1
+            }
+        ]
+    };
+
     render(){
         return (
             <div className="game-container">
-                <Inventory/>
+                <Inventory items={this.state.inventory}/>
                 <GameText/>
-                <Options list={["Continue walking", "Inspect the object"]} onSelection={(e) => alert(e)}/>
+                <Options list={this.state.activeOptions} onSelection={(e) => alert(e)}/>
             </div>
         );
     }
 }
 
 export class Inventory extends React.Component{
+    renderItems(){
+        if(typeof this.props.items != "object"){
+            return;
+        }
+
+        var itms = this.props.items.slice();
+        const mappedItems = itms.map((value, key) => {
+            return (
+                <InventoryItem key={key} id={value.id}/>
+            );
+        });
+
+        return mappedItems;
+    }
+
     render(){
         return (
             <div className="inventory">
-                
+                {this.renderItems()}
+            </div>
+        );
+    }
+}
+
+export class InventoryItem extends React.Component{
+    state = {
+        itemName: "< Unknown >",
+        itemDesc: "< Unknown >",
+        isHovered: false,
+        hoverX: 0,
+        hoverY: 0,
+    };
+
+    constructor(props){
+        super(props);
+
+        if(GameData.Items[this.props.id]){
+            this.state.itemName = GameData.Items[this.props.id].Name;
+            this.state.itemDesc = GameData.Items[this.props.id].Desc;
+        }
+    }
+
+    handleMouseHover = ((hover) => {
+        this.setState({
+            isHovered: hover
+        });
+    }).bind(this);
+
+    handleMouseMove = ((event) => {
+        this.setState({
+            hoverX: event.clientX,
+            hoverY: event.clientY
+        })
+    }).bind(this);
+
+    render(){
+        return (
+            <div
+              className="item"
+              onMouseEnter={() => this.handleMouseHover(true)}
+              onMouseLeave={() => this.handleMouseHover(false)}
+              onMouseMove={this.handleMouseMove}
+            >
+                {this.state.itemName}
+                <div
+                  className="hover-block" 
+                  style={{
+                    display: this.state.isHovered ? "block" : "none",
+                    top: this.state.hoverY + 5,
+                    left: this.state.hoverX + 5
+                  }}
+                >
+                    <h3>{this.state.itemName}</h3>
+                    <p>{this.state.itemDesc}</p>
+                </div>
             </div>
         );
     }
@@ -32,11 +119,7 @@ export class GameText extends React.Component{
     }
 }
 
-export class Options extends React.Component{
-    constructor(){
-        super();
-    }
-    
+export class Options extends React.Component{    
     handleKeyDown = ((e) => {
         if(!isNaN(e.key)){
             this.handleSelection(e.key)
@@ -60,7 +143,7 @@ export class Options extends React.Component{
             return;
         }
         
-        this.props.onSelection(num);
+        this.props.onSelection(num - 1);
     }
 
     renderOptions(){
@@ -69,7 +152,6 @@ export class Options extends React.Component{
         }
 
         var lst = this.props.list.slice();
-
         const mappedList = lst.map((value, key) => {
             let num = key + 1;
 
---
+++ src/js/gamedata/items.js
@@ -0,0 +1,12 @@
+var Items = {
+    0: {
+        Name: "Spellbook",
+        Desc: "A book that contains many powerful spells."
+    },
+    1: {
+        Name: "Bow & Arrow",
+        Desc: "A wooden bow and a quiver full of arrows."
+    }
+}
+
+export default Items;
